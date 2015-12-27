c7478cebd5312f8c85f72c35a9c49d50d9e5e9b9
Andrew Gaspar <andrew.gaspar@outlook.com>
2015-12-27 11:37:17 -0800

    Add in git branch usage

--- git/mod/Git.Reflection.psm1
+++ git/mod/Git.Reflection.psm1
@@ -64,11 +64,61 @@ function Read-GitCommandParameter
     
 }
 
+$commandsWithNonStandardOutput = @{
+    branch = @"
+usage: git branch [<options>] [-r | -a] [--merged | --no-merged]
+   or: git branch [<options>] [-l] [-f] <branch-name> [<start-point>]
+   or: git branch [<options>] [-r] (-d | -D) <branch-name>...
+   or: git branch [<options>] (-m | -M) [<old-branch>] <new-branch>
+
+Generic options
+    -v, --verbose         show hash and subject, give twice for upstream branch
+    -q, --quiet           suppress informational messages
+    -t, --track           set up tracking mode (see git-pull(1))
+    --set-upstream        change upstream info
+    -u, --set-upstream-to <upstream>
+                          change the upstream info
+    --unset-upstream      Unset the upstream info
+    --color[=<when>]      use colored output
+    -r, --remotes         act on remote-tracking branches
+    --contains <commit>   print only branches that contain the commit
+    --abbrev[=<n>]        use <n> digits to display SHA-1s
+
+Specific git-branch actions:
+    -a, --all             list both remote-tracking and local branches
+    -d, --delete          delete fully merged branch
+    -D                    delete branch (even if not merged)
+    -m, --move            move/rename a branch and its reflog
+    -M                    move/rename a branch, even if target exists
+    --list                list branch names
+    -l, --create-reflog   create the branch's reflog
+    --edit-description    edit the description for the branch
+    -f, --force           force creation, move/rename, deletion
+    --no-merged <commit>  print only not merged branches
+    --merged <commit>     print only merged branches
+    --column[=<style>]    list branches in columns
+"@
+}
+
+function Get-GitCommandHelpMessage
+{
+    Param([string]$CommandName)
+    
+    if($commandsWithNonStandardOutput[$CommandName])
+    {
+        $commandsWithNonStandardOutput[$CommandName]  -split "`n"
+    }
+    else
+    {
+        git $CommandName -h
+    }
+}
+
 function Get-GitCommandParameter
 {
     Param([string]$CommandName)
     
-    git $CommandName -h 2>&1 | Read-GitCommandParameter
+    Get-GitCommandHelpMessage $CommandName | Read-GitCommandParameter
 }
 
 class GitCommand
