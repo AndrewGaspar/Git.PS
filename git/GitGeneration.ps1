3b90a0a0b4639e00c898d4c569b6b922be181e74
Andrew Gaspar <andrew.gaspar@outlook.com>
2015-12-27 21:22:39 -0700

    Refactor git generation command conversion

--- git/GitGeneration.ps1
+++ git/GitGeneration.ps1
@@ -16,66 +16,71 @@ Import-Module "$scriptPath\mod\Git.Reflection.psm1"
 
 $preGeneration = Get-Content $preGenerationLocation | ConvertFrom-Json
 
-$sub_commands = Get-GitCommand | 
-    ForEach-Object {
-        $command_obj = New-Object PSCustomObject -Property @{
-            command = $_.Name
-        }
-        
-        if($_.AliasedTo.Count -gt 0)
-        {
-            $command_obj | Add-Member -NotePropertyName aliased_to -NotePropertyValue $_.AliasedTo.CommandName
-        }
-        else
-        {
-            $parameters = $_.Parameters |
-                Where-Object {
-                    $_
-                } |
-                ForEach-Object {
-                    if($_.LongParameter)
-                    {
-                        $parameter = $_.LongParameter
-                    }
-                    else
-                    {
-                        $parameter = $_.ShortParameter
-                    }
-                    
-                    $parameter_obj = New-Object PSCustomObject -Property @{
-                        name = $parameter
-                    }
-                    
-                    if($_.Description)
-                    {
-                        $parameter_obj | Add-Member -NotePropertyName tooltip -NotePropertyValue $_.Description
-                    }
-                    
-                    if($_.LongParameter -and $_.ShortParameter)
-                    {
-                        $parameter_obj | Add-Member -NotePropertyName alias -NotePropertyValue $_.ShortParameter
-                    }
-                    
-                    if($_.ArgumentName)
-                    {
-                        $parameter_obj | Add-Member -NotePropertyName argument_type -NotePropertyValue $_.ArgumentName
-                        
-                        if($_.IsArgumentOptional)
-                        {
-                            $parameter_obj | Add-Member -NotePropertyName argument_optional -NotePropertyValue $_.IsArgumentOptional
-                        }
-                    }
-                    
-                    $parameter_obj
+function TransformGitCommand($gitCommand)
+{
+    $command_obj = New-Object PSCustomObject -Property @{
+        command = $gitCommand.Name
+    }
+    
+    if($gitCommand.AliasedTo.Count -gt 0)
+    {
+        $command_obj | Add-Member -NotePropertyName aliased_to -NotePropertyValue $_.AliasedTo.CommandName
+    }
+    else
+    {
+        $parameters = $gitCommand.Parameters |
+            Where-Object {
+                $_
+            } |
+            ForEach-Object {
+                if($_.LongParameter)
+                {
+                    $parameter = $_.LongParameter
+                }
+                else
+                {
+                    $parameter = $_.ShortParameter
                 }
                 
-            if($parameters)
-            {
-                $command_obj | Add-Member -NotePropertyName parameters -NotePropertyValue $parameters
+                $parameter_obj = New-Object PSCustomObject -Property @{
+                    name = $parameter
+                }
+                
+                if($_.Description)
+                {
+                    $parameter_obj | Add-Member -NotePropertyName tooltip -NotePropertyValue $_.Description
+                }
+                
+                if($_.LongParameter -and $_.ShortParameter)
+                {
+                    $parameter_obj | Add-Member -NotePropertyName alias -NotePropertyValue $_.ShortParameter
+                }
+                
+                if($_.ArgumentName)
+                {
+                    $parameter_obj | Add-Member -NotePropertyName argument_type -NotePropertyValue $_.ArgumentName
+                    
+                    if($_.IsArgumentOptional)
+                    {
+                        $parameter_obj | Add-Member -NotePropertyName argument_optional -NotePropertyValue $_.IsArgumentOptional
+                    }
+                }
+                
+                $parameter_obj
             }
+            
+        if($parameters)
+        {
+            $command_obj | Add-Member -NotePropertyName parameters -NotePropertyValue $parameters
         }
-        
-        $command_obj
+    }
+    
+    $command_obj
+}
+
+$sub_commands = Get-GitCommand | 
+    ForEach-Object {
+        TransformGitCommand $_
     }
     
 $sub_commands = & {
